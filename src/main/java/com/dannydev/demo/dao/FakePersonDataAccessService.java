9e7ea008aed566011a52e1842b82617e87f76445
Ayomikun Adegoke <ayomikunadegoke@gmail.com>
2021-02-16 16:09:02 +0100

    Rest Api Starter

---
+++ src/main/java/com/dannydev/demo/dao/FakePersonDataAccessService.java
@@ -0,0 +1,20 @@
+package com.dannydev.demo.dao;
+
+import com.dannydev.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository("fakeDao")
+public class FakePersonDataAccessService implements PersonDao{
+
+    private static List<Person> db = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        db.add(new Person(id, person.getName()));
+        return 1;
+    }
+}
---
+++ src/main/java/com/dannydev/demo/service/PersonService.java
@@ -0,0 +1,22 @@
+package com.dannydev.demo.service;
+
+import com.dannydev.demo.dao.PersonDao;
+import com.dannydev.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+    }
+}
